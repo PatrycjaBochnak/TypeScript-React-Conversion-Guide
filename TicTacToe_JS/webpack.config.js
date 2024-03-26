b4f6af93ae85bc7ae9d85fabe0ddd4901cda30e1
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-03-26 15:41:43 +0100

    preparing co compile to tsx

--- TicTacToe_JS/webpack.config.js
+++ TicTacToe_JS/webpack.config.js
@@ -1,18 +1,26 @@
 module.exports = {
+  // change to .tsx if necessary
   entry: './src/app.jsx',
   output: {
     filename: './bundle.js'
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    // changed from extensions: [".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      // changed from { test: /\.jsx?$/, use: { loader: 'babel-loader' }, exclude: /node_modules/ },
+      { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+
+      // addition - add source-map support
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" }
     ]
   },
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
   },
+  // addition - add source-map support
+  devtool: "source-map"
 }
